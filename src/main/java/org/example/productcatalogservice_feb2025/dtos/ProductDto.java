c1f86295eca22af88d8df1740a4323c03c674c0f
khannaanurag1 <khannaanurag1@gmail.com>
2025-03-19 08:41:32 +0530

    Mock Mvc Test

--- src/main/java/org/example/productcatalogservice_feb2025/dtos/ProductDto.java
+++ src/main/java/org/example/productcatalogservice_feb2025/dtos/ProductDto.java
@@ -1,10 +1,12 @@
 package org.example.productcatalogservice_feb2025.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductDto {
     Long id;
     String name;
